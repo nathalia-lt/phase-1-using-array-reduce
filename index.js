17c5366c403069ad3a30fb83e1cf0361932c5e50
nathalia <nathaliatroina@gmail.com>
2022-01-13 13:48:58 -0500

    commit tree 3b3f39ad179bf2dec6dc390a07701d6c939228c9

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const bat = (accumulator, battery) => {
+    return accumulator + battery
+}
+const initialValue = 0
+const totalBatteries = batteryBatches.reduce(bat, initialValue)
